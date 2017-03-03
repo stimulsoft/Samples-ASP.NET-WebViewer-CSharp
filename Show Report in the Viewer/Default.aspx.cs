801fde7134f920d9629cb1d3c3135cc69f1038dc
Vladimir <hotchenko@gmail.com>
2017-03-03 12:20:23 +0300

    add references to nuget

--- Show Report in the Viewer/Default.aspx.cs
+++ Show Report in the Viewer/Default.aspx.cs
@@ -6,6 +6,13 @@ namespace Show_Report_in_the_Viewer
 {
     public partial class Default : System.Web.UI.Page
     {
+        public Default()
+        {
+            //Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnO...";
+            //Stimulsoft.Base.StiLicense.LoadFromFile("license.key");
+            //Stimulsoft.Base.StiLicense.LoadFromStream(stream);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
